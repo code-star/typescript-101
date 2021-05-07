95ae5acebd72602e22476094fa95370d4f44c1c8
Martin van Dam <martin.van.dam@ordina.nl>
2021-05-07 13:03:35 +0200

    Made Example 1 simpler

--- exercises/1_js_to_Typescript.js
+++ exercises/1_js_to_Typescript.js
@@ -1,67 +1,25 @@
 // Exercise 1 - Convert from JavaScript to TypeScript
 //
-// Take this file and turn it into a TypeScript file. Make it more typesafe by adding and creating types where you can identify them.
+// Take this file and turn it into a TypeScript file. Add the missing types to the variables and functions.
 //
 
-var customers = [
-	{
-		name: 'Jan',
-		lastName: 'Koper',
-		company: 'Koophandel B.V.',
-		address: {
-			city: 'Utrecht',
-			street: 'Utrechtsestraat 2',
-			zipCode: '1234 AB'
-		},
-		role: 'Chief Buying Officer',
-		age: 46
-	},
-	{
-		name: 'Henk',
-		lastName: 'Koopman',
-		company: 'Koophandel B.V.',
-		address: {
-			city: 'Utrecht',
-			street: 'Utrechtsestraat 2',
-			zipCode: '1234 AB'
-		},
-		role: 'Chief Sales Officer'
-	},
-	{
-		name: 'Freek',
-		lastName: 'Koning',
-		company: 'Koophandel B.V.',
-		address: {
-			city: 'Utrecht',
-			street: 'Utrechtsestraat 2',
-			zipCode: '1234 AB'
-		},
-		role: 'Owner',
-		age: 68
-	}
-]
+var firstName = 'Henk'
+var age = 33
+var hasChildren = false
+var parents = ['John', 'Mary']
+var parentAges = [55, 48]
+var variousValues = ['22', 21, 98, '33', '11']
 
-function getName(customer) {
-	return customer.name + ' ' + customer.lastName
+var userInfo = {
+	name: firstName,
+	age: age,
+	hasChildren: hasChildren
 }
 
-function getFullAddress(customer) {
-	return customer.address.street + ', ' + customer.address.city
+function isAdult(age) {
+	return age > 18
 }
 
-function getAge(customer) {
-	if (!customer.age) {
-		return -1
-	}
-	return customer.age
+function makeString(input) {
+	return input.toString()
 }
-
-function isCloseToRetirement(customer) {
-	return getAge(customer) > 67
-}
-
-console.log(getAge(customers[1]))
-console.log(getFullAddress(customers[0]))
-customers.forEach(function(c) {
-	console.log(getName(c), isCloseToRetirement(c))
-})
